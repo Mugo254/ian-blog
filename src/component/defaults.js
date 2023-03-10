c26296f578a30377204f281224c767efee1e1434
Mugo254 <jemo496@gmail.com>
2023-03-10 11:25:37 +0300

    added footer and also restructured the file structure

---
+++ src/component/defaults.js
@@ -0,0 +1,4 @@
+export {default as Trending} from './trending/Trending'
+export {default as Footer} from './footer/Footer'
+export {default as Recommendation} from './recommendation/Recommendation'
+export {default as TopStories} from './topStories/TopStories'
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,38 @@
-import RegistrationForm from '../component/Registration';
-import { useEffect, useState } from 'react';
+// import RegistrationForm from '../component/Registration';
+// import { useEffect, useState } from 'react';
 
-function Home() {
-  const [showModal, setShowModal] = useState(false);
+// function Home() {
+//   const [showModal, setShowModal] = useState(false);
 
-  useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      setShowModal(true);
-    }, 5000);
-    return () => clearTimeout(timeoutId);
-  }, []);
+//   useEffect(() => {
+//     const timeoutId = setTimeout(() => {
+//       setShowModal(true);
+//     }, 5000);
+//     return () => clearTimeout(timeoutId);
+//   }, []);
 
+//   return (
+//     <div>
+//       <h1>Welcome to the Homepage</h1>
+//       <RegistrationForm showModal={showModal} setShowModal={setShowModal} />
+//     </div>
+//   );
+// }
+
+// export default Home;
+
+import React from 'react'
+import {TopStories, Footer, Recommendation,Trending} from '../component/defaults'
+
+const Home = () => {
   return (
     <div>
-      <h1>Welcome to the Homepage</h1>
-      <RegistrationForm showModal={showModal} setShowModal={setShowModal} />
+      <TopStories />
+      <Trending />
+      <Recommendation />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
